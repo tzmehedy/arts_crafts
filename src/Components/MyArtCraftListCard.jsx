69c02a295d1fd4d2262884f8a33bbb590e606b19
unknown <mehedytouhidurzaman@gmail.com>
2024-10-15 17:04:48 +0600

    delete functionality completed with sweet alert

--- src/Components/MyArtCraftListCard.jsx
+++ src/Components/MyArtCraftListCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { CiEdit } from 'react-icons/ci';
+import { MdDelete } from 'react-icons/md';
+import Swal from "sweetalert2";
 
 const MyArtCraftListCard = ({ artCraftList }) => {
     const {
+      _id,
       name,
       subCategory,
       description,
@@ -12,6 +16,43 @@ const MyArtCraftListCard = ({ artCraftList }) => {
       stockStatus,
       photoUrl,
     } = artCraftList;
+
+  const handelDelete =()=>{
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/myArtCraftListDelete/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+
+
+
+
+
+    
+
+  }
+
+
+    
   return (
     <div>
       <div className=" bg-base-300 max-w-96 shadow-xl mt-5 rounded-2xl p-5">
@@ -21,8 +62,15 @@ const MyArtCraftListCard = ({ artCraftList }) => {
         <div className="card-body">
           <h2 className="card-title">Name: {name}</h2>
           <p>Sub-Category: {subCategory}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-secondary">Show Details</button>
+          <div className="card-actions justify-end gap-5">
+            <button className="">
+              {" "}
+              <CiEdit />
+            </button>
+            <button onClick={handelDelete} className="">
+              {" "}
+              <MdDelete />
+            </button>
           </div>
         </div>
       </div>
